208c941b1c80bd0543f7bebe87806f33ec40a6e4
임가현 <dlga723@naver.com>
2023-08-03 01:07:11 +0900

    frontSignUp

--- src/components/signUp.vue
+++ src/components/signUp.vue
@@ -3,105 +3,103 @@
   <article class="contents">
     <div class="signUpCard">
       <!-- 카드형식으로 만들기 위해 전체를 묶음-->
+      <form @submit.prevent="submitForm">
+        <div class="signUp">
+          <!-- 제일 윗 줄 -->
+          <span style="font-size: 18px"><b>회원가입</b></span>
+          <span style="font-size: 12px; color: gray"
+            >* 필수입력 정보입니다.</span
+          >
+        </div>
 
-      <div class="signUp">
-        <!-- 제일 윗 줄 -->
-        <span style="font-size: 18px"><b>회원가입</b></span>
-        <span style="font-size: 12px; color: gray">* 필수입력 정보입니다.</span>
-      </div>
+        <div class="inputCard">
+          <!-- 두번째 줄 -->
+          <input class="choice" type="file" accept="image/" />
+          <!-- 업로드 할 파일 선택 -->
+          <button class="upload">학생증 사진 업로드</button>
+          <!-- 선택된 파일 업로드 버튼 -->
+        </div>
 
-      <div class="inputCard">
-        <!-- 두번째 줄 -->
-        <input class="choice" type="file" accept="image/" />
-        <!-- 업로드 할 파일 선택 -->
-        <button class="upload">학생증 사진 업로드</button>
-        <!-- 선택된 파일 업로드 버튼 -->
-      </div>
+        <div class="inputCard" id="inputId">
+          <!-- 세번째 줄 -->
+          <input
+            class="email"
+            type="email"
+            id="email"
+            name="email"
+            placeholder="id를 입력하세요 *"
+          />
+          <!-- 아이디를 email로 표현 -->
+          <button type="button" class="duplicateCheck">중복확인</button>
+          <!-- 중복확인 버튼 -->
+          <!-- 데이터베이스에 아이디 있으면 alert로 중복표시? -->
+        </div>
 
-      <div class="inputCard" id="inputId">
-        <!-- 세번째 줄 -->
-        <input
-          class="email"
-          type="email"
-          id="email"
-          name="email"
-          placeholder="id를 입력하세요 *"
-        />
-        <!-- 아이디를 email로 표현 -->
-        <button type="button" class="duplicateCheck">중복확인</button>
-        <!-- 중복확인 버튼 -->
-        <!-- 데이터베이스에 아이디 있으면 alert로 중복표시? -->
-      </div>
+        <div class="inputCard">
+          <!-- 네번째 줄 -->
+          <input
+            type="text"
+            id="nickName"
+            name="nickName"
+            placeholder="사용하실 닉네임을 입력하세요 *"
+          />
+          <!-- 닉네임 입력 공간 -->
+        </div>
 
-      <div class="inputCard">
-        <!-- 네번째 줄 -->
-        <input
-          type="text"
-          id="nickName"
-          name="nickName"
-          placeholder="사용하실 닉네임을 입력하세요 *"
-        />
-        <!-- 닉네임 입력 공간 -->
-      </div>
+        <div class="inputCard">
+          <!-- 다섯번째 줄 -->
+          <input
+            type="password"
+            id="password"
+            name="password"
+            required
+            placeholder="비밀번호를 입력하세요 *"
+          />
+          <!-- 비밀번호 입력 공간 -->
+        </div>
 
-      <div class="inputCard">
-        <!-- 다섯번째 줄 -->
-        <input
-          type="password"
-          id="password"
-          name="password"
-          required
-          placeholder="비밀번호를 입력하세요 *"
-        />
-        <!-- 비밀번호 입력 공간 -->
-      </div>
+        <div class="inputCard">
+          <!-- 여섯번째 줄 -->
+          <input
+            type="password"
+            id="checkPassword"
+            name="checkPassword"
+            placeholder="비밀번호를 다시 입력하세요 *"
+          />
+          <!-- 비밀번호확인 입력 공간 -->
+        </div>
 
-      <div class="inputCard">
-        <!-- 여섯번째 줄 -->
-        <input
-          type="password"
-          id="checkPassword"
-          name="checkPassword"
-          placeholder="비밀번호를 다시 입력하세요 *"
-        />
+        <div class="inputCard">
+          <!-- 여섯번째 줄 -->
+          <div class="dropdown">
+            <label for="employmentStatus"> 취업 상태 : </label>
+            <select id="employmentStatus" v-model="selectedEmploymentStatus">
+              <option
+                v-for="status in employmentStatusOptions"
+                :key="status"
+                :value="status"
+              >
+                {{ status }}
+              </option>
+            </select>
+          </div>
+
+          <div class="dropdown">
+            <label for="category"> 분야 : </label>
+            <select id="category" v-model="selectedCategory">
+              <option v-for="item in categoryOptions" :key="item" :value="item">
+                {{ item }}
+              </option>
+            </select>
+          </div>
+        </div>
+
         <!-- 비밀번호확인 입력 공간 -->
-      </div>
 
-      <div class="inputCard">
-        <!-- 여섯번째 줄 -->
-        <div class="dropdown">
-          <button class="dropbtn" @click="toggleEmploymentStatus">
-            {{ employmentStatus }}
-          </button>
-          <div class="dropdown-content" v-show="showEmploymentStatus">
-            <a
-              v-for="status in employmentStatusOptions"
-              :key="status"
-              @click="selectEmploymentStatus(status)"
-              >{{ status }}</a
-            >
-          </div>
+        <div class="submit">
+          <button class="submitBtn">가입하기</button>
         </div>
-        <div class="dropdown">
-          <button class="dropbtn" @click="toggleCategory">
-            {{ category }}
-          </button>
-          <div class="dropdown-content" v-show="showCategory">
-            <a
-              v-for="item in categoryOptions"
-              :key="item"
-              @click="selectCategory(item)"
-              >{{ item }}</a
-            >
-          </div>
-        </div>
-      </div>
-
-      <!-- 비밀번호확인 입력 공간 -->
-
-      <div class="submit">
-        <button class="submitBtn">가입하기</button>
-      </div>
+      </form>
     </div>
   </article>
 </template>
@@ -111,13 +109,12 @@ export default {
   name: "cheerupSignup",
   data() {
     return {
-      employmentStatus: "취업", // 초기값 설정
       showEmploymentStatus: false, // 드롭다운 토글 변수
       employmentStatusOptions: ["취업", "취업준비생"], // 취업 상태 옵션들
 
-      category: "기계", // 초기값 설정
       showCategory: false, // 드롭다운 토글 변수
       categoryOptions: [
+        "-",
         "개발",
         "경영마케팅",
         "제조업",
@@ -129,6 +126,15 @@ export default {
         "교육",
         "기타",
       ], // 카테고리 옵션들
+
+      //받아올 데이터 입력받을 곳
+      choice: "",
+      email: "",
+      nickName: "",
+      password: "",
+      checkPassword: "",
+      employmentStatus: "",
+      category: "",
     };
   },
   methods: {
@@ -151,6 +157,18 @@ export default {
       this.category = item;
       this.showCategory = false;
     },
+    submitForm() {
+      const userData = {
+        //eslint-disable-line no-unused-vars
+        choice: this.choice,
+        email: this.email,
+        nickName: this.nickName,
+        password: this.password,
+        checkPassword: this.checkPassword,
+        employmentStatus: this.employmentStatus,
+        category: this.category,
+      };
+    },
   },
 };
 </script>
@@ -166,6 +184,10 @@ article {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+::placeholder {
+  padding-left: 5px;
 }
 
 .signUpCard {
